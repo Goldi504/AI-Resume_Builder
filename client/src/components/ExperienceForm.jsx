7bbefe771819822451cd079a3540b42b8c412bfc
Goldi504 <goldikumari107@gmail.com>
2026-06-08 15:39:06 +0530

    update experienceForm

--- client/src/components/ExperienceForm.jsx
+++ client/src/components/ExperienceForm.jsx
@@ -1,4 +1,4 @@
-import { Briefcase, Sparkles, Trash , Plus, Loader2 } from "lucide-react";
+import { Briefcase, Sparkles, Trash, Plus, Loader2 } from "lucide-react";
 import React from "react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
@@ -6,8 +6,8 @@ import api from "../configs/api";
 import toast from "react-hot-toast";
 
 const ExperienceForm = ({ data, onChange }) => {
-  const {token} = useSelector(state => state.auth)
-  const [generatingIndex, setIsGeneratingIndex] = useState(-1)
+  const { token } = useSelector((state) => state.auth);
+  const [generatingIndex, setIsGeneratingIndex] = useState(-1);
   const addExperience = () => {
     const newExperience = {
       company: "",
@@ -30,22 +30,25 @@ const ExperienceForm = ({ data, onChange }) => {
     onChange(updated);
   };
 
-  const generateDescription = async(index) =>{
-    setIsGeneratingIndex(index)
-    const experience = data[index]
+  const generateDescription = async (index) => {
+    setIsGeneratingIndex(index);
+    const experience = data[index];
     const prompt = `enhance this job description ${experience.description} for the 
-    position of ${experience.position} at ${experience.company}.`
+    position of ${experience.position} at ${experience.company}.`;
 
     try {
-      const {data} = await api.post('/api/ai/enhance-jon-desc', {userContent: prompt}, {headers:{Authorization:token}})
-      updateExperience(index, "description" , data.enhancedContent)
+      const { data } = await api.post(
+        "/api/ai/enhance-job-desc",
+        { userContent: prompt },
+        { headers: { Authorization: `Bearer ${token}` } },
+      );
+      updateExperience(index, "description", data.enhancedContent);
     } catch (error) {
-      toast.error(error.message)
+      toast.error(error.message);
+    } finally {
+      setIsGeneratingIndex(-1);
     }
-    finally{
-      setIsGeneratingIndex(-1)
-    }
-  }
+  };
 
   return (
     <div className="space-y-6">
@@ -63,7 +66,8 @@ const ExperienceForm = ({ data, onChange }) => {
         <button
           onClick={addExperience}
           className="flex items-center gap-2 px-3 py-1 text-sm bg-green-100 text-green-700 rounded-lg hover:bg-purple-200
-           transition-colors">
+           transition-colors"
+        >
           <Plus className="size-4" />
           Add Experience
         </button>
@@ -133,43 +137,54 @@ const ExperienceForm = ({ data, onChange }) => {
               </div>
 
               <label className="flex items-center gap-2">
-                <input type="checkbox"
-                checked={experience.is_current || false}
-                onChange={(e) => {
-                  updateExperience(
-                    index,
-                    "is_current",
-                    e.target.checked ? true : false,
-                  );
-                }}
-                className="rounded border border-gray-300 text-blue-600 focus:ring-blue-500"
-              />
+                <input
+                  type="checkbox"
+                  checked={experience.is_current || false}
+                  onChange={(e) => {
+                    updateExperience(
+                      index,
+                      "is_current",
+                      e.target.checked ? true : false,
+                    );
+                  }}
+                  className="rounded border border-gray-300 text-blue-600 focus:ring-blue-500"
+                />
                 <span className="text-sm text-gray-700">
                   Currently working here
                 </span>
               </label>
               <div className="space-y-2">
                 <div className="flex items-center justify-between">
-                    <label className="text-sm font-medium text-gray-700">Job Description</label>
-                    <button onClick={()=> generateDescription(index)} disabled={generatingIndex === index || !experience.position || 
-                      !experience.company} className="flex items-center gap-1 px-2 py-1 text-xs bg-purple-200 text-purple-700 rounded
-                     hover:bg-purple-200 transition-colors disabled:opacity-50"> 
-                     {generatingIndex === index ? (
+                  <label className="text-sm font-medium text-gray-700">
+                    Job Description
+                  </label>
+                  <button
+                    onClick={() => generateDescription(index)}
+                    disabled={
+                      generatingIndex === index ||
+                      !experience.position ||
+                      !experience.company
+                    }
+                    className="flex items-center gap-1 px-2 py-1 text-xs bg-purple-200 text-purple-700 rounded
+                     hover:bg-purple-200 transition-colors disabled:opacity-50"
+                  >
+                    {generatingIndex === index ? (
                       <Loader2 className=" w-3 h-3 animate-spin" />
-                     ):(
-                        <Sparkles className="w-3 h-3"/>
-
-                     )}
-                        Enhance with AI
-                    </button>
-
+                    ) : (
+                      <Sparkles className="w-3 h-3" />
+                    )}
+                    Enhance with AI
+                  </button>
                 </div>
-                
-                    <textarea value={experience.description || ""} onChange={(e)=> updateExperience
-                        (index,"description" , e.target.value)} className="w-full text-sm px-3 py-2 rounded-lg resize-none
-                     placeholder= Describe your key responsibilities and achievements..." />
-                
 
+                <textarea
+                  value={experience.description || ""}
+                  onChange={(e) =>
+                    updateExperience(index, "description", e.target.value)
+                  }
+                  className="w-full text-sm px-3 py-2 rounded-lg resize-none
+                     placeholder= Describe your key responsibilities and achievements..."
+                />
               </div>
             </div>
           ))}
